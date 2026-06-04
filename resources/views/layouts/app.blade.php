9a55d88f7c8193e40dd198d1eaf4bbac96e30d03
azizbinsam <abdulazizbinsambas@gmail.com>
2026-06-04 16:55:11 +0700

    feat: add SEO config and layout meta tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    {!! SEOMeta::generate() !!}
+    {!! OpenGraph::generate() !!}
+    {!! TwitterCard::generate() !!}
     <title>{{ config('app.name', 'Delix Studio Store') }} - @yield('title', 'Home')</title>
     <link rel="icon" type="image/png" href="/favicon.png">
 
